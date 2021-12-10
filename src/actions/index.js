fb97b892fefcbffc1d742684351475486227115e
andrewhunt21 <andrewhunt21@icloud.com>
2021-12-10 15:02:35 -0800

    actions mostly complete

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,5 +1,46 @@
 import axios from 'axios';
 
+export const FETCH_START = "FETCH_START"; 
+export const FETCH_SUCCESS = "FETCH_SUCCESS";
+export const FETCH_FAIL = "FETCH_FAIL";
+export const ADD_SMURF = "ADD_SMURF";
+export const ADD_FAIL = "ADD_FAIL";
+
+export const fetchSmurfs = () => {
+    return (dispatch) => {
+        dispatch(fetchStart());
+
+        // props.fetchStart();
+        axios.get('http://localhost:3333/smurfs')
+            .then(resp => {
+                console.log(resp);
+                // dispatch(fetchSuccess(resp.data[0]));
+            }).catch(err => {
+                dispatch(fetchFail(err));
+            })
+    }
+}
+
+export const fetchStart = () => {
+    return({type: FETCH_START});
+}
+
+export const fetchSuccess = (smurf) => {
+    return({type: FETCH_SUCCESS, payload:smurf});
+}
+
+export const fetchFail = (error) => {
+    return({type: FETCH_FAIL, payload:error});
+}
+
+export const addSmurf = (smurf) => {
+    return({type: ADD_SMURF, payload:{name:smurf.name, position:smurf.position, nickname:smurf.nickname, description: smurf.description}});
+}
+
+export const addFail = () => {
+    return({type: ADD_FAIL, payload:'Name, Position, Nickname, and Description are required to submit'});
+}
+
 //Task List:
 //1. Add a thunk action called fetchSmurfs that triggers a loading status display in our application, performs an axios call to retreive smurfs from our server, saves the result of that call to our state and shows an error if one is made.
 //2. Add a standard action that allows us to add new smurf (including the name, nickname, position, summary)
